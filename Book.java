027f13065389cb126a275f8f13b2027ce864d76f
herbertcode <168911501+herbertcode@users.noreply.github.com>
2024-12-18 10:47:41 -1200

    Book.java

--- Book.java
+++ Book.java
@@ -1,29 +1,25 @@
 package org.example;
 
 public class Book {
-    //Initializing Variables
+    
     private String title;
     private String author;
     private int yearPublished;
-    private boolean isBorrowed = false;
-
-    //Constructor Function
+    
     public Book(String title, String author, int yearPublished) {
         this.title = title;
         this.author = author;
         this.yearPublished = yearPublished;
     }
-
-    //Title Getter and Setter
+    
     public String getTitle() {
         return title;
     }
-
+    
     public void setTitle(String title) {
         this.title = title;
     }
-
-    //Author Getter and Setter
+    
     public String getAuthor() {
         return author;
     }
@@ -32,7 +28,6 @@ public class Book {
         this.author = author;
     }
 
-    //YearPublished Getter and Setter
     public int getYearPublished() {
         return yearPublished;
     }
@@ -41,25 +36,8 @@ public class Book {
         this.yearPublished = yearPublished;
     }
 
-    //isBorrowed getter and setter
-    public boolean getIsBorrowed() {
-    return isBorrowed;
-}
-
-    public void setIsBorrowed() {
-    isBorrowed = !isBorrowed;
-}
-
-
-    //Method to return a book
-    public boolean returnBook() {
-        if (!isBorrowed) {
-            System.out.println("This book is not borrowed");
-            return false;
-        } else {
-            System.out.println("This book has been returned");
-            isBorrowed = false;
-            return true;
-        }
+    @Override
+    public String toString() {
+        return "Book [Title: " + title + ", Author: " + author + ", Year Published: " + yearPublished + "]";
     }
 }
