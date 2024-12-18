805643aee7b090dd4f96f0adf6e12cb5e74304f5
herbertcode <168911501+herbertcode@users.noreply.github.com>
2024-12-18 11:11:46 -1200

    Book.java

--- Book.java
+++ Book.java
@@ -1,43 +1,70 @@
 package org.example;
 
+import java.util.Objects;
+
 public class Book {
-    
+
     private String title;
     private String author;
     private int yearPublished;
+
     
     public Book(String title, String author, int yearPublished) {
         this.title = title;
         this.author = author;
         this.yearPublished = yearPublished;
     }
+
     
     public String getTitle() {
         return title;
     }
+
     
     public void setTitle(String title) {
         this.title = title;
     }
-    
+
+   
     public String getAuthor() {
         return author;
     }
 
+    
     public void setAuthor(String author) {
         this.author = author;
     }
 
+    
     public int getYearPublished() {
         return yearPublished;
     }
 
+    
     public void setYearPublished(int yearPublished) {
         this.yearPublished = yearPublished;
     }
 
+    
     @Override
     public String toString() {
         return "Book [Title: " + title + ", Author: " + author + ", Year Published: " + yearPublished + "]";
+    }
+
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Book book = (Book) obj;
+        return yearPublished == book.yearPublished &&
+               Objects.equals(title, book.title) &&
+               Objects.equals(author, book.author);
+    }
+
+   
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, author, yearPublished);
     }
 }
